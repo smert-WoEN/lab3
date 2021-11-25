96996c936e83e4ebe4ba89331492af14e58f090d
DmitryDenisov <denisdmit03@gmail.com>
2021-11-25 17:41:18 +0300

    add ChangeRoom

---
+++ src/doing/ChangeRooms.java
@@ -0,0 +1,15 @@
+package doing;
+
+import superclasses.ChangeRoom;
+import superclasses.Human;
+import superclasses.Room;
+
+public class ChangeRooms implements ChangeRoom {
+    @Override
+    public void changeRoom(Human human, Room room1, Room room2) {
+        if (human != null && room1 != null && room2 != null){
+            room1.deleteHumanFromRoom(human);
+            room2.addHumanInRoom(human);
+        }
+    }
+}
---
+++ src/superclasses/ChangeRoom.java
@@ -0,0 +1,5 @@
+package superclasses;
+
+public interface ChangeRoom {
+    void changeRoom(Human human, Room room1, Room room2);
+}
